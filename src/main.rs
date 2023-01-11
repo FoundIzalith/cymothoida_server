10fb50a67fca7ec1120d439066cd363f82acebfa
FoundIzalith <b17kaylaeva@gmail.com>
2023-01-10 19:54:06 -0500

    Add basic structure

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,43 @@
+use std::{
+    fs, 
+    io::{prelude::*, BufReader},
+    net::{TcpListener, TcpStream},
+    thread,
+    time::Duration,
+}
+use cymothoida_server::User;
+
 fn main() {
-    println!("Hello, world!");
+    println!("Starting new Cymothoida server...");
+
+    let ip = 127.0.0.1:7878;
+    let max_users = 8;
+
+    let listener = TcpListener::bind(ip).unwrap();
+    let users = User::new(max_users);
+
+    for stream in listener.incoming() {
+        let stream = stream.unwrap();
+
+        users.execute(|| {
+            handleConnection(stream);
+        });
+    }
+}
+
+fn handleConnection(mut stream: TcpStream) {
+    let buf_reader = BufReader::new(&mut stream);
+    let request_line = buf_reader.lines().next().unwrap().unwrap();
+
+    println!("New connection");
+
+}
+
+//Send messages to connected clients
+fn sendMsg(message: String) {
+
+}
+
+fn processCommand(message: String) {
+
 }
